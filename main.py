81c827266239881e49ff0c71be6f48187b23d3cd
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-09-04 21:30:24 +0100

    addedBoxesAndDetection.

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ img = cv2.imread("E:/ObjectCounter/SimpleObjectCounter/puppies.jpg")
 img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.figure(figsize=(10, 10))
 plt.axis("off")
-plt.imshow(img1)
+# plt.imshow(img1)
+
+# Creating boxes, labels around the objects
+box, label, count = cv.detect_common_objects(img)
+output = draw_bbox(img, box, label, count)
+
+output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10, 10))
+plt.axis("off")
+plt.imshow(output)
+plt.show()
 
