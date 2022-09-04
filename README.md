1ceb262f674e6af3ddec8cf4833b593ee0882e36
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-09-04 20:38:41 +0100

    fixingGit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# SimpleObjectCounter
+ - Using openCv library to make a simple objects detector and count them, then printing them to the conosle.
---
+++ main.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+from numpy.lib.polynomial import poly
