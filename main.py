d3ba3c18c7da3290cded01f6cf5a9266fa408a10
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-09-04 21:22:31 +0100

    loadingViewingImage.

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 from numpy.lib.polynomial import poly
 
-#Loading and Viewwing the Image
+# Loading and Viewwing the Image
+img = cv2.imread("E:/ObjectCounter/SimpleObjectCounter/puppies.jpg")
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10, 10))
+plt.axis("off")
+plt.imshow(img1)
+
